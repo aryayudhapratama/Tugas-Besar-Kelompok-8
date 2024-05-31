4750645e4fd7fdd445d2f512b00b85a127263a18
Rafi Syeghani Ardiyanto <syeghanirafi@gmail.com>
2024-05-31 20:44:19 +0700

    Mmebuat login regis,halaman shop,navbar,footer

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,65 @@
+    <!-- Start Footer -->
+    <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Sunshine Shoes Care</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            Telkom University Surabaya
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none" href="tel:010-020-0340">666-666-6666</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none" href="sunshineshoescare@gmail.com">sunshineshoescare@gmail.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Tubes P. Framework</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam perspiciatis ipsam vero delectus modi voluptatibus voluptate. Molestiae, suscipit consequuntur.</p></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Anggota Kelompok</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>Arya Yudha | ...</li>
+                        <li>Sandi Adimas | ...</li>
+                        <li>Ferdian Bimo | ...</li>
+                        <li>Rafi Syeghani | 1204220047</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row text-light mb-4">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+                <div class="col-auto me-auto">
+                    <ul class="list-inline text-left footer-icons">
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="bi bi-tiktok"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="bi bi-whatsapp"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="w-100 bg-black py-3">
+        </div>
+    </footer>
+    <!-- End Footer -->
