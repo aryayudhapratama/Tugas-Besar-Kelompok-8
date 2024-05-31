96439ced06b04062d9fa76778b3b29b052c57d44
unknown <aryayudhapratama2003@gmail.com>
2024-05-31 20:55:23 +0700

    Membuat tampilan halaman admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route Admin
+Route::get('admin', function () {
+    return view('admin');
+});
+Route::get('table', function () {
+    return view('table');
+});
+
