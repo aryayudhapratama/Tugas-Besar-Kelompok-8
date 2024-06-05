95d668c0e84034da16148c898c7ef28e975980ee
unknown <aryayudhapratama2003@gmail.com>
2024-06-05 21:14:34 +0700

    Membenarkan route

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,7 +14,7 @@
                 <div class="navbar-nav ms-auto d-flex justify-content-end">
                     <ul class="nav navbar-nav d-flex align-items-center">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('home') }}">Home</a>
+                            <a class="nav-link" href="{{ url('/') }}">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('shop') }}">Shop</a>
